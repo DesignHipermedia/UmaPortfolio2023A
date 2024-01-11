36e2fadaa84fb07c9a429ce0ee2a1ca2fca721e3
Tiagoytsk <tiagompedra@gmail.com>
2024-01-11 04:28:29 +0000

    projeto v1.1 com mobile

--- Tiagoytsk/java.js
+++ Tiagoytsk/java.js
@@ -1,6 +1,6 @@
-<script>
-const cardContainer = document.getElementById("card-container");
-const loadMoreButton = document.getElementById("load-more");
-const cardCountElem = document.getElementById("card-count");
-const cardTotalElem = document.getElementById("card-total");
+<script setup>
+  import { ref } from "vue";
+  function notify() {
+    alert('You Have been subscribed to the newsletter.')
+  }
 </script>
