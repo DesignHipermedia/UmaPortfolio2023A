9cc330ff7d6dd734eb90840742364be7ae2575f1
its-mtvw <94139986+its-mtvw@users.noreply.github.com>
2024-01-15 03:05:42 +0000

    Add files via upload

---
+++ its-mtvw/projeto design/script.js
@@ -0,0 +1,11 @@
+const darkModeBtn = document.getElementById('dark-mode-btn');
+const lightModeBtn = document.getElementById('light-mode-btn');
+const body = document.body;
+
+darkModeBtn.addEventListener('click', () => {
+    body.classList.add('dark-mode');
+});
+
+lightModeBtn.addEventListener('click', () => {
+    body.classList.remove('dark-mode');
+});
